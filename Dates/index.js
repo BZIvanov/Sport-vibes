93f38229508c224ffc6d310fcd888a3e8570d773
BZIvanov <ivanov.biser@gmx.com>
2021-03-29 16:12:36 +0300

    med date format example

--- Dates/index.js
+++ Dates/index.js
@@ -11,3 +11,7 @@ console.log(DateTime.local().minus({ day: 1 }).toISODate()); // 2021-03-11
 console.log(DateTime.local().minus({ week: 1 }).toISODate()); // 2021-03-05
 
 console.log(DateTime.fromISO('2021-03-14')); // DateTime.js
+
+// the provided date is different time zone, but toLocaleString will also set to current time zone
+const myLocalDate = DateTime.fromISO('2021-03-29T19:21:37.950Z');
+console.log(myLocalDate.toLocaleString(DateTime.DATETIME_MED)); // Mar 29, 2021, 10:21 PM
